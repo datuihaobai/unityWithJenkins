fd3271507cea55303fd4446f25c5832985873d1a
datui <84951134@qq.com>
2020-02-04 15:16:12 +0800

    测试打包变量的传递

--- Assets/Editor/BuildPlayer.cs
+++ Assets/Editor/BuildPlayer.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BuildPlayer
 {
     public static void TestCallByBatch()
     {
-        Debug.LogWarning("TestCallByBatch");
+        Debug.Log("TestCallByBatch Start");
+
+        string[] args = Environment.GetCommandLineArgs();
+        Debug.Log("CommandLineArgs");
+        foreach (var arg in args)
+            Debug.Log(arg);
+
+        IDictionary vars = Environment.GetEnvironmentVariables();
+        Debug.Log("EnvironmentVariables");
+        foreach (var var in vars)
+            Debug.Log(var);
+
+        Debug.Log("TestCallByBatch End");
     }
 }
