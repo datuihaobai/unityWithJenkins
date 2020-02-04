5684d8e8408f0f6356a44f2e51186404de18c8d3
datui <84951134@qq.com>
2020-02-04 12:25:33 +0800

    add BuildPlayer.cs

---
+++ Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlayer
+{
+    public static void TestCallByBatch()
+    {
+        Debug.LogWarning("TestCallByBatch");
+    }
+}
